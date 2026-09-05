da343adcad63812ca25438e5f4b504506d800fbb
qawsilkop <qawsilkop@gmail.com>
2026-09-05 18:56:51 +0800

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: '籃卡進出管理系統',
+  description: 'Basket Management System',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-TW">
+      <body className="bg-slate-50 text-slate-800">{children}</body>
+    </html>
+  );
+}
